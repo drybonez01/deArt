cb4f51fbeb67cea4efdf7f47e84d816c0d969115
drybonez01 <83592470+drybonez01@users.noreply.github.com>
2023-01-17 20:14:13 +0100

    commit

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-//var Migrations = artifacts.require("./Migrations.sol");
-
-const Migrations = artifacts.require("Migrations");
-
-module.exports = function(deployer) {
-    deployer.deploy(Migrations);
-};
